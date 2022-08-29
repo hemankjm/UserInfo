2ee6d9ffc61afed389b21b13485feb682eb61a06
SOYA95 <66457249+SOYA95@users.noreply.github.com>
2022-08-29 20:41:47 +0900

    url추가

--- src/main/java/petfriends/config/WebConfig.java
+++ src/main/java/petfriends/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer{
 		// TODO Auto-generated method stub
 		WebMvcConfigurer.super.addCorsMappings(registry);
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:3000")
+		.allowedOrigins("http://k8s-default-petfrien-e22d96b656-549281536.us-west-2.elb.amazonaws.com:80", "http://localhost:3000")
 		.allowedMethods(HttpMethod.GET.name(),
 				        HttpMethod.POST.name(),
 				        HttpMethod.PUT.name(),
