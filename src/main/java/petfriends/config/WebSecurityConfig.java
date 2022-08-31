562a929943747aac0f215be61189247c07b96fc6
heman1004 <heman1004@gamil.com>
2022-09-01 00:28:42 +0900

    edit

--- src/main/java/petfriends/config/WebSecurityConfig.java
+++ src/main/java/petfriends/config/WebSecurityConfig.java
@@ -76,7 +76,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
           // 로그인, 회원가입 API 는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
           .and()
           .authorizeRequests()
-          .antMatchers("/auths/signup/**","/auths/login/**","/swagger/**","/swagger-ui.html","/swagger-resources/**","/webjars/**","/v2/api-docs").permitAll()
+          .antMatchers("/userInfos/me/**","/auths/signup/**","/auths/login/**","/swagger/**","/swagger-ui.html","/swagger-resources/**","/webjars/**","/v2/api-docs").permitAll()
           .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
           // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
